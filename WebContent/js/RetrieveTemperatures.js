68b36591b2d446980131b2ebaa125399ef6a260f
marcelstaro <marcelstaro@gmail.com>
2016-04-17 14:01:04 -0400

    added bootstrap styles

--- WebContent/js/RetrieveTemperatures.js
+++ WebContent/js/RetrieveTemperatures.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
     $("button").click(function(){
-        $.post("https://temperature-tracker.herokuapp.com/Temperature.json",
+        $.post("http://localhost:8080/TemperatureTrackerLocal/Temperature.json",
         {
-          username: "marcel",
-          password: "mypass0123"
+          username: $("#username").val(),
+          password: $("#password").val()
         },
         function(data,status){
         	
         	$.each(data.temperature, function(index, element) {
         		$("#result").append('<p>');
-        		$("#result").append('</br>id: ' + element.id);
+        		$("#result").append('</br>id test: ' + element.id);
         		$("#result").append('</br>Temperature: ' + element.temp);
         		$("#result").append('</br>Date: ' + element.date);
         		$("#result").append('</p>');
